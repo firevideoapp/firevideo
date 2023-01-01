7667b38fe15efb2e90cd6b454496c723e5535deb
firevideoapp <firevideoapp@gmail.com>
2023-01-01 22:45:12 +0530

    yd

---
+++ assets/js/movie_details.js
@@ -0,0 +1,82 @@
+const api_url = "https://firevideo.vercel.app/DB/MOVIES.json";
+const providers_api_url = "https://firevideo.vercel.app/DB/providers.json";
+const hypershorts_api_url = "https://hypershort.com/api?api=29b3ea95cb95add2b6696337d055174877afee13&url=";
+
+let movieImage = document.getElementById('movie_image');
+let genreLabel = document.getElementById('movie_genre');
+let storyText = document.getElementById('storyline');
+let studioText = document.getElementById('studio_title');
+let watchButton = document.getElementById('watch_btn');
+
+var playerShowing = false;
+
+var providerName, streamUrl;
+
+async function getMovieData(id) {
+    const response = await fetch(api_url);
+    var data = await response.json();
+    data.forEach((item) => {
+        let movie = item;
+        if (movie.id === id) {
+            document.getElementById('movie_image').src = movie.banner;
+            document.getElementById('movie_title').textContent = movie.name;
+            // document.getElementById('movie_thumb').style.background = "linear-gradient(to left, transparent 10%, black 60%),url(" + movie.thumbnail + ") no-repeat";
+            // document.getElementById('movie_thumb').style.backgroundSize = 'cover';
+            genreLabel.innerHTML = movie.genre;
+            storyText.innerHTML = movie.description;
+            getProviderNameById(movie.provider.split(',')[0]);
+            streamUrl = movie.stream;
+
+            if (!streamUrl.includes('/e/')) {
+                var lastOccurrence = streamUrl.lastIndexOf("/");
+                streamUrl = streamUrl.substring(0, lastOccurrence + 1) + "e" + streamUrl.substring(lastOccurrence);
+            }
+
+            watchButton.style.display = 'block';
+        }
+    });
+}
+
+function watchButtonClick() {
+    let iframe = document.createElement('iframe');
+    iframe.id = 'embed_player';
+    iframe.src = streamUrl;
+    iframe.allowFullscreen = true;
+    iframe.frameBorder = 0;
+    iframe.style.position = 'absolute';
+    // iframe.style.top = 0;
+    // iframe.style.bottom = 0;
+    // iframe.style.right = 0;
+    // iframe.style.left = 0;
+    iframe.width = 640;
+    iframe.height = 360;
+    document.body.appendChild(iframe);
+    // document.getElementById('header').style.display = "none";
+    playerShowing = true;
+    // document.location.href = streamUrl;
+}
+
+async function getDownloadUrl(url) {
+    const response = await fetch(hypershorts_api_url + streamUrl.replace('/e/', '/d/'));
+    var data = await response.json();
+    var shortenedUrl = data.shortenedUrl;
+    document.location.href = shortenedUrl;
+}
+
+async function getProviderNameById(id) {
+    const response = await fetch(providers_api_url);
+    var data = await response.json();
+    // let providerName;
+    data.forEach((item) => {
+        let provider = item;
+        if (provider.id == id) {
+            studioText.innerHTML = provider.name;
+        } else {
+            studioText.innerHTML = "FireVideo";
+        }
+    });
+}
+
+let params = (new URL(document.location)).searchParams;
+let id = params.get('id');
+window.addEventListener("load", getMovieData(id), false);
--- assets/js/movies.js
+++ assets/js/movies.js
@@ -1,22 +1,72 @@
-const api_url = "https://api.streamsb.co/api/folder/list?key=9027dcim62hq4asqpoa6&fld_id=37648";
+const api_url = "https://firevideo.vercel.app/DB/MOVIES.json";
 async function getFilesAndFolders() {
     const response = await fetch(api_url);
     var data = await response.json();
-    let list = document.getElementById("myList");
+    let list = document.getElementById("movie_list");
     // var jsonData = await JSON.parse(data);
-    data.result.folders.forEach((item) => {
-        let name = item.name;
-        let link = document.createElement('a');
-        link.href = "";
-        link.onclick = "onClick(item.fld_id)"; //"https://api.streamsb.co/api/folder/list?key=9027dcim62hq4asqpoa6&fld_id=" + item.fld_id;
-        link.innerText = name;
-        link.style.color = "#ffffff";
-        list.appendChild(link);
-    });
+    for(let i = 0; i < 25; i++) {
+        var movie = data[i];
+        let li = document.createElement('li');
+
+        let movieCard = document.createElement('div');
+        movieCard.className = 'movie-card';
+
+        let url = './movie-details.html?id=' + movie.id;
+        
+        let clickable = document.createElement('a');
+        clickable.href = url;
+
+        let figure = document.createElement('figure');
+        figure.className = 'card-banner';
+
+        let image = document.createElement('img');
+        image.src = movie.banner;
+        image.alt = movie.name;
+
+        figure.appendChild(image);
+
+        clickable.appendChild(figure);
+
+        let titleWrapper = document.createElement('div');
+        titleWrapper.className = 'title-wrapper';
+
+        let clickableTitle = document.createElement('a');
+        clickableTitle.href = url;
+
+        let h3 = document.createElement('h3');
+        h3.className = 'card-title';
+        h3.textContent = movie.name;
+
+        let p = document.createElement('p');
+        p.className = 'normal-text-thin';
+        // p.style.color = '#ffffff';
+        p.innerHTML = movie.language;
+        
+        clickableTitle.appendChild(h3);
+
+        titleWrapper.appendChild(clickableTitle);
+
+        movieCard.appendChild(clickable);
+
+        movieCard.appendChild(titleWrapper);
+        movieCard.appendChild(p);
+
+        li.appendChild(movieCard);
+
+        list.appendChild(li);
+
+        // let name = movie.name;
+        // let link = document.createElement('a');
+        // link.href = "";
+        // link.onclick = "onClick(movie.fld_id)"; //"https://api.streamsb.co/api/folder/list?key=9027dcim62hq4asqpoa6&fld_id=" + movie.fld_id;
+        // link.innerText = name;
+        // link.style.color = "#ffffff";
+        // list.appendChild(link);
+    }
 }
 
-function onClick() {
-
+function onClick(fld_id) {
+    
 }
 
 if (window.addEventListener) {
