5db0aef9413ab6276f36fbad5c48cf21491c994a
firevideoapp <firevideoapp@gmail.com>
2023-03-04 22:52:07 +0530

    yd

--- movies/category.js
+++
@@ -1,9 +0,0 @@
-const response = await fetch("https://firevideo.vercel.app/DB/MOVIES.json")
-
-const names = await response.json()
-
-const reqListener = function(req, res) {
-    res.setHeader("Content-Type", "application/json");
-    res.writeHead(200);
-    res.end(JSON.stringify(names, null, 3));
-}
