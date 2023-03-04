f85d098f574fc55ebc1482bf427f2cb805c322bb
firevideoapp <firevideoapp@gmail.com>
2023-03-04 21:19:20 +0530

    yd

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.contentType = "application/json";
+    res.send({
+        'Name': 'Hello World'
+    });
+});
+
+app.get('/movies/category', (req, res) => {
+    res.contentType = "application/json";
+    res.send(getMovies());
+});
+
+async function getMovies() {
+    const response = await fetch('https://firevideo.vercel.app/DB/MOVIES.json')
+    return response;
+}
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
